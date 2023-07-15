af01d33b259a52c875f206b415ccc0f8a807aeee
Fethiye <fethiye0623@gmail.com>
2023-07-15 14:18:44 +0200

    test case updated..

--- src/test/java/cw/tests/AmazonTestPage.java
+++ src/test/java/cw/tests/AmazonTestPage.java
@@ -12,25 +12,27 @@ public class AmazonTestPage {
 
 
     /*
-o The https://www.amazon.com.tr/ site is opened.
-o Check that the main page is opened.
-o Accept cookies is selected from the cookie preferences.
-o Login to the site.
-o Login process is checked.
-o Computer is selected from the categories tab next to the search button.
-o Check that the Computer category is selected.
-o Write msi in the search field and search.
-o Check that the search has been made.
-o The second page opens from the search results page.
-o Check that the 2nd page is opened.
-o 2nd product on the page is added to cart.
-o 2. Check that the product has been added to cart.
-o Check that the “Shopping Cart” page is opened.
-o Added product is deleted from cart.
-o It is checked that the deletion process has taken place.
-o Member exit process is done.
-o Check that the exit process is done.
-     */
+ 1.The https://www.amazon.com.tr/ site is opened.
+ 2.Check that the main page is opened.
+ 3.Accept cookies is selected from the cookie preferences.
+ 4.Login to the site.
+ 5.Login process is checked.
+ 6.Computer is selected from the categories tab next to the search button.
+ 7.Check that the Computer category is selected.
+ 8.Write msi in the search field and search.
+ 9.Check that the search has been made.
+ 10.The second page opens from the search results page.
+ 11.Check that the 2nd page is opened.
+ 12.2nd product on the page is added to cart.
+ 13.Check that the product has been added to cart.
+ 14.Check that the “Shopping Cart” page is opened.
+ 15.Number of items purchased in the basket{quantity} is increased
+ 16.Cart amount must be found as product price*quantity
+ 17.Added product is deleted from cart.
+ 18.It is checked that the deletion process has taken place.
+ 19.Member exit process is done.
+ 20.Check that the exit process is done.
+    */
 
 
     @Test
@@ -104,8 +106,6 @@ o Check that the exit process is done.
 
 //         Check that the exit process is done.
         Assert.assertTrue(cartPage.eMailOrPhoneArea.isDisplayed(), "user did not sign out");
-
-
 
 
     }
