4275fe6a18d4a63553363956d0ef28a6be8ceb99
Fethiye <fethiye0623@gmail.com>
2023-07-23 20:51:56 +0200

    AmazonBasePage aktualisiert..

--- src/test/java/cw/pages/amazon/AmazonBasePage.java
+++ src/test/java/cw/pages/amazon/AmazonBasePage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 
@@ -26,8 +24,6 @@ public class AmazonBasePage {
     public void loginClick() {
 
         Driver.getDriver().findElement(By.xpath("//a[@class='nav-a']"));
-       // Driver.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']")).click();
-
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(Driver.getDriver().findElement(By.xpath("(//a[@id='nav-link-accountList'])[1]"))).perform();
         Driver.getDriver().findElement(By.xpath("//a[@class='nav-a']")).click();
@@ -40,7 +36,6 @@ public class AmazonBasePage {
         WebElement searchDropdown = Driver.getDriver().findElement(By.xpath("//select[@name='url']"));
         Select select = new Select(searchDropdown);
         select.selectByVisibleText("Computers");
-
 
     }
 
@@ -56,57 +51,11 @@ public class AmazonBasePage {
     }
 
 
-    public void abmelden() {
+    public void abMelden() {
         WebElement abmelden = Driver.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']"));
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(abmelden).perform();
         Driver.getDriver().findElement(By.xpath("//span[.='Sign Out']")).click();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    protected static int productCount = 4;
-    //Ich habe ein statisches int zugewiesen, da die Anzahl der Mengen und die Anzahl der Produkte gleich sein müssen
-
-    public AmazonBasePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
-
-    @FindBy(id = "twotabsearchtextbox")
-    public WebElement textSearch;
-
-    @FindBy(className = "nav-cart-icon nav-sprite")
-    public WebElement cartButton;
-
-    @FindBy(xpath = "//h1[@class='a-spacing-mini a-spacing-top-base']")
-    public WebElement cartIsEmptyMessage;
-
-
-    public void searchFor(String keyword) {
-        textSearch.sendKeys(keyword + Keys.ENTER);
-    }
-
-
-     */
-
 
